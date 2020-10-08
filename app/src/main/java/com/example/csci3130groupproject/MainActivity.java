6a337d52a53ff1f700aa187bb0e428acc0a3c87e
Dajel Bourque <dj654130@dal.ca>
2020-10-08 10:08:44 -0300

    Registration button is clicked: feature

--- app/src/main/java/com/example/csci3130groupproject/MainActivity.java
+++ app/src/main/java/com/example/csci3130groupproject/MainActivity.java
@@ -2,16 +2,30 @@ package com.example.csci3130groupproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button login, register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button_register).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                launchRegisterActivity();
+            }
+        });
+
     }
+
+    private void launchRegisterActivity(){
+        Intent intent = new Intent(this, RegistrationActivity.class);
+        startActivity(intent);
+    }
+
 }
