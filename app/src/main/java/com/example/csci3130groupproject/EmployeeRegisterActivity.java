b3cb0e2509c3ef8227e1b984dc52ea0fe93fc07e
Dajel Bourque <dj654130@dal.ca>
2020-10-08 12:18:40 -0300

    Registration Employee/Employer button pressed: feature

---
+++ app/src/main/java/com/example/csci3130groupproject/EmployeeRegisterActivity.java
@@ -0,0 +1,14 @@
+package com.example.csci3130groupproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EmployeeRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employee_register);
+    }
+}
--- app/src/main/java/com/example/csci3130groupproject/RegistrationActivity.java
+++ app/src/main/java/com/example/csci3130groupproject/RegistrationActivity.java
@@ -13,6 +13,32 @@ public class RegistrationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
+        //Clicking Employer button - launches EmployerRegisterActivity
+        findViewById(R.id.button_employer).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                launchEmployerRegisterActivity();
+            }
+        });
+
+        //Clicking Employee button - launches EmployeeRegisterActivity
+        findViewById(R.id.button_employee).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                launchEmployeeRegisterActivity();
+            }
+        });
+
+    }
+
+    private void launchEmployerRegisterActivity(){
+        Intent intent = new Intent(this, EmployerRegisterActivity.class);
+        startActivity(intent);
+    }
+
+    private void launchEmployeeRegisterActivity(){
+        Intent intent = new Intent(this, EmployeeRegisterActivity.class);
+        startActivity(intent);
     }
 
 }
