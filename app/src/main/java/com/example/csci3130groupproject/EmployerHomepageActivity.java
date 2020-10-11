99628a4453c4d442ac6923babdca9ac8cfa699ee
Dajel Bourque <dj654130@dal.ca>
2020-10-11 11:05:27 -0300

    SubmitTaskActivity access: feature

--- app/src/main/java/com/example/csci3130groupproject/EmployerHomepageActivity.java
+++ app/src/main/java/com/example/csci3130groupproject/EmployerHomepageActivity.java
@@ -2,7 +2,9 @@ package com.example.csci3130groupproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class EmployerHomepageActivity extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class EmployerHomepageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employer_homepage);
+
+        findViewById(R.id.button_homepageSubmitTask).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                launchSubmitTaskActivity();
+            }
+        });
+
     }
+
+    private void launchSubmitTaskActivity(){
+        Intent intent = new Intent(this, SubmitTaskActivity.class);
+        startActivity(intent);
+    }
+
 }
