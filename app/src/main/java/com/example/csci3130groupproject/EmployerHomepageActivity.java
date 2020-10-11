858119b2e99309e44a5b9a24b5b97a56c047e236
Dajel Bourque <dj654130@dal.ca>
2020-10-11 10:50:23 -0300

    Employer Homepage access: feature

--- app/src/main/java/com/example/csci3130groupproject/EmployerHomepageActivity.java
+++ app/src/main/java/com/example/csci3130groupproject/EmployerHomepageActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class EmployerHomepage extends AppCompatActivity {
+public class EmployerHomepageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/csci3130groupproject/EmployerRegisterActivity.java
+++ app/src/main/java/com/example/csci3130groupproject/EmployerRegisterActivity.java
@@ -2,7 +2,9 @@ package com.example.csci3130groupproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class EmployerRegisterActivity extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class EmployerRegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employer_register);
+
+        findViewById(R.id.button_signUpEmployer).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                launchEmployerHomepageActivity();
+            }
+        });
+
     }
+
+    private void launchEmployerHomepageActivity(){
+        Intent intent = new Intent(this, EmployerHomepageActivity.class);
+        startActivity(intent);
+    }
+
 }
