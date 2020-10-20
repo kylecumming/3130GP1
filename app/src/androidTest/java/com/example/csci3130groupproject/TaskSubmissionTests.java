45649ae4ca980b60c4b9918507d415bb0f807a8a
Dajel Bourque <dj654130@dal.ca>
2020-10-20 14:20:14 -0300

    Refactor: moved task submission tests to one test class

--- app/src/androidTest/java/com/example/csci3130groupproject/TaskSubmissionTests.java
+++ app/src/androidTest/java/com/example/csci3130groupproject/TaskSubmissionTests.java
@@ -1,6 +1,13 @@
 package com.example.csci3130groupproject;
 
+import androidx.annotation.NonNull;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -9,6 +16,7 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.*;
 import static androidx.test.espresso.assertion.ViewAssertions.*;
 import static androidx.test.espresso.matcher.ViewMatchers.*;
+import static org.junit.Assert.assertTrue;
 
 public class TaskSubmissionTests {
     @Rule
@@ -20,7 +28,32 @@ public class TaskSubmissionTests {
         onView(withId(R.id.button_register)).perform(click());
         onView(withId(R.id.button_employer)).perform(click());
         onView(withId(R.id.button_signUpEmployer)).perform(click());
-        onView(withId(R.id.button_createTask)).perform(click());
+        onView(withId(R.id.button_homepageSubmitTask)).perform(click());
         onView(withId(R.id.button_submitTask)).check(matches(isDisplayed()));
     }
+
+    @Test
+    public void taskInFirebase(){
+        onView(withId(R.id.button_register)).perform(click());
+        onView(withId(R.id.button_employer)).perform(click());
+        onView(withId(R.id.button_signUpEmployer)).perform(click());
+        onView(withId(R.id.button_homepageSubmitTask)).perform(click());
+        onView(withId(R.id.edittext_taskTitle)).perform(typeText("TestTitle"));
+        onView(withId(R.id.edittextmulti_taskDescription)).perform(typeText("This is a description"),closeSoftKeyboard());
+        onView(withId(R.id.edittext_taskPayment)).perform(typeText("100"), closeSoftKeyboard());
+        onView(withId(R.id.button_submitTask)).perform(click());
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference taskNameRef = database.getReference().child("taskName").child("-MJwuSFZhsjhekDlDZWC");
+        taskNameRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String value = snapshot.getValue().toString();
+                assertTrue(value.equals("TestTitle"));
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+    }
+
 }
