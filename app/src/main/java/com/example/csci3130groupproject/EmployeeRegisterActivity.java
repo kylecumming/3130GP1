b24546b784b1f2e904bc32b5510f9e102cc2da5a
Dajel Bourque <dj654130@dal.ca>
2020-10-25 13:31:21 -0300

    Create homepage for employee: test passed

--- app/src/main/java/com/example/csci3130groupproject/EmployeeRegisterActivity.java
+++ app/src/main/java/com/example/csci3130groupproject/EmployeeRegisterActivity.java
@@ -2,7 +2,9 @@ package com.example.csci3130groupproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class EmployeeRegisterActivity extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class EmployeeRegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employee_register);
+
+        findViewById(R.id.button_signUpEmployee).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchEmployeeHomepageActivity();
+            }
+        });
+
     }
+
+    private void launchEmployeeHomepageActivity(){
+        Intent intent = new Intent(this, EmployeeHomepageActivity.class);
+        startActivity(intent);
+    }
+
 }
