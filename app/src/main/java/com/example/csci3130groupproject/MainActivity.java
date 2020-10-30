2a60af80db06996750f95d5d4d780610316dcfaa
Spencer Hines <sp456145@dal.ca>
2020-10-30 01:23:38 -0300

    This commit adds the functionality  being able to click a specific task within the task list and view it in a separate page in which you can then view it in greater detail/apply for the task(applying for the task has not been implemented yet but will be in the future).

--- app/src/main/java/com/example/csci3130groupproject/MainActivity.java
+++ app/src/main/java/com/example/csci3130groupproject/MainActivity.java
@@ -31,5 +31,11 @@ public class MainActivity extends AppCompatActivity {
     public void onClick(View view){//this button will be removed later on, I am just using it as a way to skip through the menus so I can quickly test my work
         startActivity(new Intent(getApplicationContext(), ViewSingleTask.class));
     }
+    public void onClick2(View view){//this button will be removed later on, I am just using it as a way to skip through the menus so I can quickly test my work
+        startActivity(new Intent(getApplicationContext(), ViewTasksActivity.class));
+    }
+    public void onClick3(View view){//this button will be removed later on, I am just using it as a way to skip through the menus so I can quickly test my work
+        startActivity(new Intent(getApplicationContext(), SubmitTaskActivity.class));
+    }
 
 }
